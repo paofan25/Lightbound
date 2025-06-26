761cb42ecb018a3efde0e68328c7eabd461429d5
ReRoot <143421613+inrootshell@users.noreply.github.com>
2025-06-26 14:22:25 +0800

    Add files via upload

---
+++ Assets/Scenes/Scripts/KnapsackSystem.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+//±³°üÏµÍ³
+public class KnapsackSystem : MonoBehaviour
+{
+    [SerializeField] private GameObject item;
+    [SerializeField] private GameObject context;
+
+
+}
